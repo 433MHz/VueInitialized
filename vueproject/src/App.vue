a6975201eaef3330ed3862116610fbb044955416
Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-03-15 19:02:07 +0100

    Example of full vue app generated by CLI

---
+++ vueproject/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      title: 'Hello vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+
+body{
+  text-align: center;
+}
+</style>
